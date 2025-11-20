2a2f22232a7c7a2cdbf7ab4313ad2d56f6bffc4a
ArchanHirani01 <archanhirani01@gmail.com>
2025-11-20 01:39:54 +0100

    Add Kaggle data import instructions
    Added instructions for importing Kaggle data sources and a print statement.

--- code.py
+++ code.py
@@ -1,1 +1,9 @@
+# IMPORTANT: RUN THIS CELL IN ORDER TO IMPORT YOUR KAGGLE DATA SOURCES,
+# THEN FEEL FREE TO DELETE THIS CELL.
+# NOTE: THIS NOTEBOOK ENVIRONMENT DIFFERS FROM KAGGLE'S PYTHON
+# ENVIRONMENT SO THERE MAY BE MISSING LIBRARIES USED BY YOUR
+# NOTEBOOK.
+import kagglehub
+elemento_nyc_yellow_taxi_trip_data_path = kagglehub.dataset_download('elemento/nyc-yellow-taxi-trip-data')
 
+print('Data source import complete.')
